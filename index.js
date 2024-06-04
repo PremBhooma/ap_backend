cf3bc28ddbb67dfb3156ae156ed5df540aab980e
anujQuiz <anujsocialmediamanager@gmail.com>
2024-06-04 23:35:41 +0530

    "Removed unnecessary imports, reformatted code, and added semicolons to end of statements."

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const { connection } = require('./config/db');
+const { connection } = require("./config/db");
 
-require('dotenv').config();
-
+require("dotenv").config();
 
 const adminRouter = require("./routes/Admin.routes");
 const categoryRouter = require("./routes/Category.routes");
@@ -14,27 +11,29 @@ const aiToolRouter = require("./routes/AiTool.routes");
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: "*"
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/api/admin", adminRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/aitool", aiToolRouter);
 
 app.get("/", (req, res) => {
-    res.send("API is running");
-})
+  res.send("API is running");
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, async () => {
-    try {
-        await connection
-        console.log("DB Connected Sucessfully")
-    } catch (err) {
-        console.log("DB Failed to Connect")
-        console.log(err)
-    }
-    console.log(`Listening on ${PORT} Port`)
-})
+  try {
+    await connection;
+    console.log("DB Connected Sucessfully");
+  } catch (err) {
+    console.log("DB Failed to Connect");
+    console.log(err);
+  }
+  console.log(`Listening on ${PORT} Port`);
+});
